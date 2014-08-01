7827a0b7c6d9af42e3c3d6cd0a9c6a682f9c3289
taikoubou <godon.lykn.ows03@gmail.com>
2014-08-01 20:28:33 +0900

    spec_helper.rb git add

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'minitest'
-
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
@@ -41,5 +39,4 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  config.include Capybara::DSL
 end
