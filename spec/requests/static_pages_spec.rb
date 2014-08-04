3f35bef9f1af70f00b0d34ec3b50107e02cd5afd
taikoubou <godon.lykn.ows03@gmail.com>
2014-08-04 18:52:12 +0900

    Practice 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,32 +2,53 @@ require 'spec_helper'
 
 describe "StaticPages" do
   subject {page}
+
   let(:base_title){"Ruby on Rails Tutorial Sample App"}
+  shared_examples_for"all static pages" do
+    it { should have_content(heading)}
+    it { should have_title(full_title(page_title))}
+  end
+
   describe "Home Pages" do
     before {visit root_path}
+    let(:heading) {'Sample App'}
+    let(:page_title){''}
 
-    it { should have_content('Sample App')}
-    it { should have_title(full_title(''))}
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home')}
   end
 
   describe "Help Page" do
     before {visit help_path}
-    it { should have_content('Help')}
-    it { should have_title(full_title('Help'))}
+    let(:heading) {'Help'}
+    let(:page_title) {'Help'}
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before {visit about_path}
-
-    it { should have_content('About Us')}
-    it { should have_title(full_title('About Us'))}
+    let(:heading) {'About Us'}
+    let(:page_title) {'About Us'}
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before {visit contact_path}
+    let(:heading) {'Contact'}
+    let(:page_title) {'Contact'}
+    it_should_behave_like "all static pages"
+  end
 
-    it { should have_content('Contact')}
-    it { should have_title(full_title('Contact'))}
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
