f62babb4dc28976b7eefd00eea93bfdff3dfcb47
taikoubou <godon.lykn.ows03@gmail.com>
2014-08-26 00:45:27 +0900

    A lot of Bug Fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
-    redirect_to do |format|
+    respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
@@ -13,7 +13,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
-    redirect_to do |format|
+    respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
